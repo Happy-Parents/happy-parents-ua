12499f7512cb829d8c9a10d33522bf14d413de88
kyryloliinyk <kyryl.oliinyk@gmail.com>
2024-04-28 20:42:52 +0300

    Add basic layout with main page and language switcher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
+# Main application controller to be inherited from with all shared behaviour
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = detected_locale
+  end
+
+  def detected_locale
+    default_locale = I18n.default_locale
+    if request.path == '/admin'
+      default_locale
+    else
+      session[:locale] || default_locale
+    end
+  end
 end
