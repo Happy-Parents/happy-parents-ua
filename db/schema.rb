61aece51d3b24e9ed76c694483ad6a7de1063d70
kyryloliinyk <kyryl.oliinyk@gmail.com>
2024-07-11 12:34:52 +0300

    Add composite index on books and book_categories
    to book_categories_books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_143339) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_11_092734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_143339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_category_id"], name: "index_book_categories_books_on_book_category_id"
+    t.index ["book_id", "book_category_id"], name: "index_book_categories_books_on_book_id_and_category_id", unique: true
     t.index ["book_id"], name: "index_book_categories_books_on_book_id"
   end
 
