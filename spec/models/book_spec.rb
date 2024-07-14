875ef77bb9aa97efbf3a2666057765b89f72674c
kyryloliinyk <kyryl.oliinyk@gmail.com>
2024-07-14 14:47:10 +0300

    Allow Book to delegate #in_stock? to Product
    Add tests for all delgations to Product

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -22,6 +22,24 @@ RSpec.describe Book do
     it { is_expected.to have_and_belong_to_many(:book_categories) }
   end
 
+  describe 'delegations' do
+    %i[drop_shipping_available
+       drop_shipping_available?
+       inventory_number
+       name
+       name_uk
+       name_ru
+       price
+       published
+       published?
+       slug
+       stock_balance
+       manufacturer
+       in_stock?].each do |method|
+      it { is_expected.to delegate_method(method).to(:product) }
+    end
+  end
+
   describe 'validations' do
     %i[cover_type language pages_count].each do |attribute|
       it { is_expected.to validate_presence_of(attribute) }
